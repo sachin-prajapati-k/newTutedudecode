6046661bd1e4b4deb8a85837372d85186d6b7637
sachin <sdev77577@gmail.com>
2025-12-19 21:38:28 +0530

    fgsdfgsd

--- src/Movie/Components/Navbar/index.tsx
+++ src/Movie/Components/Navbar/index.tsx
@@ -3,9 +3,17 @@ import "./index.css";
 export default function NavBar() {
   return (
     <>
-      <div className="container-fluid bg-purple-custom">
-        <div>Logo</div>
-        <div className="brand-name">BrandName</div>
+      <div className="container-fluid bg-purple-custom d-flex align-items-center justify-content-between h-100 p-3">
+        <div className="site-brand align-items-center d-flex gap-2">
+          <div>Logo</div>
+          <div className="brand-name">BrandName</div>
+        </div>
+        <div className=" gap-2 row border me-4 ">
+          <div className="col-auto border">Home</div>
+          <div className="col-auto border">Movies</div>
+          <div className="col-auto border">Songs</div>
+          <div className="col-auto border">Web Series</div>
+        </div>
       </div>
     </>
   );
