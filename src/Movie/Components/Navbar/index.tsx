0acdefb3d7308a291b0560e71398f900e65be8c6
sachin <sdev77577@gmail.com>
2025-12-20 19:22:23 +0530

    dfgdfgdfg

--- src/Movie/Components/Navbar/index.tsx
+++ src/Movie/Components/Navbar/index.tsx
@@ -3,16 +3,32 @@ import "./index.css";
 export default function NavBar() {
   return (
     <>
-      <div className="container-fluid bg-purple-custom d-flex align-items-center justify-content-between h-100 p-3">
+      <div className="container-fluid bg-purple-custom d-flex align-items-center justify-content-between h-100 p-3 ">
         <div className="site-brand align-items-center d-flex gap-2">
           <div>Logo</div>
           <div className="brand-name">BrandName</div>
         </div>
-        <div className=" gap-2 row border me-4 ">
-          <div className="col-auto border">Home</div>
-          <div className="col-auto border">Movies</div>
-          <div className="col-auto border">Songs</div>
-          <div className="col-auto border">Web Series</div>
+        <div className=" gap-2 row  me-4 ">
+          <div className="nav-item col-auto  btn-round">
+            <a href="www.google.com" target="blank" className="nav-link">
+              Home
+            </a>
+          </div>
+          <div className="nav-item col-auto btn-round">
+            <a href="www.google.com" className="nav-link">
+              Songs 
+            </a>
+          </div>
+          <div className="nav-item col-auto  btn-round">
+            <a href="www.google.com" className="nav-link">
+              Web Series
+            </a>
+          </div>
+          <div className="nav-item col-auto  btn-round">
+            <a href="www.google.com" className="nav-link">
+              TV Shows
+            </a>
+          </div>
         </div>
       </div>
     </>
