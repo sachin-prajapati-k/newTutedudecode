6e7c23815b375826ea1c5cf2eb6568498612f640
sachin <sdev77577@gmail.com>
2025-12-21 12:05:07 +0530

    asdasd

--- src/Movie/Components/Search&filterButtons/index.tsx
+++ src/Movie/Components/Search&filterButtons/index.tsx
@@ -1,12 +1,23 @@
-export default function SearchFilter() {
+import { useState } from "react";
+
+export default function SearchFilter({ Title = "Bollywod Hits" }: any) {
+    const [searchText,setSearchText]=useState('')
+    
   return (
     <>
-      <div className="container mt-3 justify-content-center text-white">
-        <input
-          className="form-control w-lg-50 w-sm-25"
-          type="text"
-          placeholder="Searh here"
-        />
+      <div className="mt-3 container flex-column align-items-center text-white ">
+        <div className="m-0">
+          <h1 style={{color:"yellow"}}>{Title}</h1>
+        </div>
+        <div className="container p-0 justify-content-center">
+          <input
+            className="form-control w-50 w-lg-50 w-sm-25"
+            type="text"
+            placeholder="Searh here"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <p className="mt-1">Founded no. results for "{searchText}"</p>
       </div>
     </>
   );
